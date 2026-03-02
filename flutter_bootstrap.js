28bcc383e21dabf9638f9207f4a4066196180acb
anderbaher-tech <tech@anderbaher.com>
2026-03-02 18:55:24 +0530

    ABTCT-382&ABTCT-403

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3879368872"
+    serviceWorkerVersion: "4029952346"
   }
 });
